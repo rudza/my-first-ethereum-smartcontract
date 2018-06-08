80ae8097d6eb6b550d5290d5885be6ba9e8cf62d
Rudolfs Janitis <rudolfs@codecraft.lv>
2018-06-08 09:17:06 +0300

    Initial commit

---
+++ contracts/Inbox.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+    
+    constructor(string initialmessage) public {
+        message = initialmessage;    
+    }
+    
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+}
