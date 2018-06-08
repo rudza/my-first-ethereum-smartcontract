ffc343f6f12dfdf87550131bab909acf128f80d8
Rudolfs Janitis <rudolfs@codecraft.lv>
2018-06-08 09:59:50 +0300

    Ad README & Minor updates

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -2,11 +2,11 @@ pragma solidity ^0.4.17;
 
 contract Inbox {
     string public message;
-    
+
     constructor(string initialmessage) public {
-        message = initialmessage;    
+        message = initialmessage;
     }
-    
+
     function setMessage(string newMessage) public {
         message = newMessage;
     }
